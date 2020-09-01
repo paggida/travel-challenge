0f1ab0aa6dd4e72ab190372c5e2b14484a323ed8
Danilo Paggi <danilopaggi@gmail.com>
2020-09-01 16:22:31 -0300

    CLI engine

--- src/index-Cli.js
+++ src/index-Cli.js
@@ -1,14 +1,28 @@
 #!/usr/bin/env node
 const program = require('commander');
+const inquirer = require('inquirer');
 const package = require('../package.json');
 
 program.version(package.version);
 
 program
-    .command('start')
-    .description('Receives the CSV file with the routes.')
-    .action(() => {
-        console.log('start');
-    });
+  .arguments('[csvFile]')
+  .action(async (csvFile) => {
+    let inputRoute={};
+
+    console.log('\nPress "Ctrl+C" to close.');
+
+    do{
+      inputRoute = await inquirer.prompt([{
+        type: 'input',
+        name: 'route',
+        message: 'Please enter the route:'
+      }]);
+
+      if(inputRoute.route!==''){
+        console.log(`Best route: ${inputRoute.route}`);
+      }
+    }while(inputRoute.route.length);
+  });
 
 program.parse(process.argv);
