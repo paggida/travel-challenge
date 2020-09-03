f9e6f89385b8dfdec9a6a443d7c90005f0717540
Danilo Paggi <danilopaggi@gmail.com>
2020-09-03 16:07:49 -0300

    Create and delete routes in CSV file

--- src/database/services/routerDBServices.js
+++ src/database/services/routerDBServices.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const es = require('event-stream');
+const { promisify } = require('util')
 const csvParse = require('csv-parse');
 const ResponseObj = require('../../app/domain/models/ResponseObj');
 
@@ -22,9 +23,36 @@ const _isDataFileNonexistent = () => !fs.existsSync(_csvDataFilePath);
 _initializeDataFile();
 
 module.exports = {
-  async setNewRoute(partnerObj) {
-    //TBD
+  async setNewRoute(newRouteString) {
+    const error = await promisify(fs.appendFile)(_csvDataFilePath, `${newRouteString}\n`);
+
+    if (error) {
+      return new ResponseObj(500, _getErrorMessage(error));
+    }
+    else {
+      return new ResponseObj(200, 'Successful route creation.');
+    }
   },
+
+  async deleteRoute(destinations=[],price) {
+    const { Data:dataFileRecordsArray} = await this.getAllRoutes();
+    const filterDataFileRecords = dataFileRecordsArray.filter(route => route[0]!==destinations[0] || route[1]!==destinations[1] || route[2]!== price.toString());
+    const recordsCsvString = filterDataFileRecords.reduce((total, route)=>`${total}${route[0]},${route[1]},${route[2]}\n`, '\n').slice(1);
+
+    if(dataFileRecordsArray.length === filterDataFileRecords.length){
+      return new ResponseObj(404, 'Route not found.');
+    }
+
+    const error = await promisify(fs.writeFile)(_csvDataFilePath, recordsCsvString, { flag: "w" });
+
+    if (error) {
+      return new ResponseObj(500, _getErrorMessage(error));
+    }
+    else {
+      return new ResponseObj(200, 'Successful delete route.');
+    }
+  },
+
   async getAllRoutes() {
     const output = [];
 
@@ -44,7 +72,7 @@ module.exports = {
         reject(new ResponseObj(500, message));
       });
       csvReader.on('end', () => {
-        resolve(output)
+        resolve(new ResponseObj(200, output));
       });
     });
   },
--- tests/unity/database/services/routerDBServices.test.js
+++ tests/unity/database/services/routerDBServices.test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const es = require('event-stream');
 const csvParse = require('csv-parse');
+const ResponseObj = require('../../../../src/app/domain/models/ResponseObj');
 const routerDBServices = require('../../../../src/database/services/routerDBServices');
 const csvDataFilePath =  path.resolve(__dirname, '..', '..', '..', '..', 'src', 'database', 'data','input-file.csv');
 
@@ -27,6 +28,64 @@ describe('Validation of the search for all routes in the CSV file.', () => {
 
     const response = await routerDBServices.getAllRoutes();
 
-    expect(response).toHaveLength(expectResponse.length);
+    expect(response).toBeInstanceOf(ResponseObj);
+    expect(response).toHaveProperty('Code', 200);
+    expect(response).toHaveProperty('Data');
+    expect(response.Data).toHaveLength(expectResponse.length);
+  });
+});
+
+describe('Validation of the route creation flow in the CSV file.', () => {
+  it('Should be able to save a new route in the CSV file.', async () => {
+    const oldRoutesData = await routerDBServices.getAllRoutes();
+
+    const createResponse = await routerDBServices.setNewRoute('TestRouteJest,TestRouteJest,99999');
+
+    const {Data:newRoutesData} = await routerDBServices.getAllRoutes();
+
+    const newRoutes = newRoutesData[newRoutesData.length-1];
+
+
+    expect(createResponse).toBeInstanceOf(ResponseObj);
+    expect(createResponse).toHaveProperty('Code', 200);
+    expect(oldRoutesData).toHaveProperty('Code', 200);
+    expect(createResponse).toHaveProperty('Data', 'Successful route creation.');
+    expect(newRoutesData.length).toBeGreaterThan(oldRoutesData.Data.length);
+    expect(newRoutes).toHaveLength(3);
+    expect(newRoutes[0]).toBe('TestRouteJest');
+    expect(newRoutes[1]).toBe('TestRouteJest');
+    expect(newRoutes[2]).toBe('99999');
+  });
+});
+
+describe('Validation of the route delete flow in the CSV file.', () => {
+  it('Should be able to delete an existing route in the CSV file.', async () => {
+    const oldRoutesData = await routerDBServices.getAllRoutes();
+
+    const deleteResponse = await routerDBServices.deleteRoute(['TestRouteJest','TestRouteJest'],99999);
+
+    const newRoutesData = await routerDBServices.getAllRoutes();
+
+    expect(deleteResponse).toBeInstanceOf(ResponseObj);
+    expect(deleteResponse).toHaveProperty('Code', 200);
+    expect(newRoutesData).toHaveProperty('Code', 200);
+    expect(oldRoutesData).toHaveProperty('Code', 200);
+    expect(deleteResponse).toHaveProperty('Data', 'Successful delete route.');
+    expect(newRoutesData.Data.length).toBeLessThan(oldRoutesData.Data.length);
+  });
+  it('Should not be able to delete a route that does not exist in the CSV file.', async () => {
+    const oldRoutesData = await routerDBServices.getAllRoutes();
+
+    const deleteResponse = await routerDBServices.deleteRoute(['TestRouteJest2','TestRouteJest2'],99999);
+
+    const newRoutesData = await routerDBServices.getAllRoutes();
+
+    expect(deleteResponse).toBeInstanceOf(ResponseObj);
+    expect(deleteResponse).toHaveProperty('Code', 404);
+    expect(newRoutesData).toHaveProperty('Code', 200);
+    expect(oldRoutesData).toHaveProperty('Code', 200);
+    expect(deleteResponse).toHaveProperty('Data');
+    expect(deleteResponse.Data).toHaveProperty('message', 'Route not found.');
+    expect(newRoutesData.Data.length).toBe(oldRoutesData.Data.length);
   });
 });
