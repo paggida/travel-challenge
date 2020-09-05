895a1ac49eac4610e8d6ea5401e2a0f1521755b8
Danilo Paggi <danilopaggi@gmail.com>
2020-09-05 09:56:51 -0300

    Adding instance tests

--- tests/unity/app/domain/models/Route.test.js
+++ tests/unity/app/domain/models/Route.test.js
@@ -1,5 +1,16 @@
 const Route = require('../../../../../src/app/domain/models/Route');
 
+describe('Validation of the instance of a Route object.', () => {
+  it('Should be able to instance a valid Route object.', () => {
+    const routeObj = new Route(['TestRouteJest1','TestRouteJest2'],99999)
+
+    expect(routeObj).toBeInstanceOf(Route);
+    expect(Object.keys(routeObj)).toHaveLength(2);
+    expect(routeObj).toHaveProperty('Destinations', ['TestRouteJest1','TestRouteJest2']);
+    expect(routeObj).toHaveProperty('Price', 99999);
+  });
+});
+
 describe('Validation of the conversions methods in a Route object.', () => {
   it('Should be able to convert a valid Route object in a CSV file record.', () => {
     const routeObj = new Route(['TestRouteJest1','TestRouteJest2'],99999)
