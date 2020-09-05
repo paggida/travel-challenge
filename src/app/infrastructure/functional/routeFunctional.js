5d3f24816bda262e04f6994661725661385f4ee9
Danilo Paggi <danilopaggi@gmail.com>
2020-09-05 19:21:23 -0300

    Function to convert RoutesArray to Adjacency List

--- src/app/infrastructure/functional/routeFunctional.js
+++ src/app/infrastructure/functional/routeFunctional.js
@@ -1,9 +1,73 @@
+const AdjacencyList = require('../../domain/models/AdjacencyList');
+
 module.exports = {
   getConvertRouteStringToArray(routeString){
     const destinations =  routeString.split('-');
     return destinations;
+  },
+  getConvertRoutesArrayToAdjacencyList(routesArray){
+    const nodeArray = [];
+    const edgesArray = [];
+
+    for(let [nodeA, nodeB, price] of routesArray){
+      const indexA = _setNodeInArrayIfNew(nodeA, nodeArray);
+      const indexB = _setNodeInArrayIfNew(nodeB, nodeArray);
+
+      _setEdgeInArray({indexOrigin:indexA, indexTarget:indexB, price}, edgesArray)
+      _setEdgeInArray({indexOrigin:indexB, indexTarget:indexA, price}, edgesArray)
+    }
+
+    return new AdjacencyList(nodeArray,edgesArray);
   }
 }
 
+//Module functions
+const _isInvalidIndex = index => index<0;
+const _isPriceAMoreExpensiveThanPriceB = (priceA, priceB) => priceA>priceB;
 
+//Node functions
+const _isFirstNodeEdge = (nodeIndex, edgesArray) => !edgesArray[nodeIndex];
+const _getNodeIndexInArray = (node, nodesArray) => nodesArray.indexOf(node);
+const _setNodeInArrayIfNew = (node, nodesArrayOutput)=>{
+  let nodeIndex =_getNodeIndexInArray(node, nodesArrayOutput);
+
+  if(_isInvalidIndex(nodeIndex)){
+    nodesArrayOutput.push(node);
+    nodeIndex = nodesArrayOutput.length-1;
+  }
+
+  return nodeIndex;
+};
+
+//Edge Functions
+const _isNewEdge = edge => !edge;
+const _getEdgeIndexInArray = (edge, nodeIndex, edgesArray) => edgesArray[nodeIndex].indexOf(edge);
+const _getEdgeNodeIndexToNodeIndexInArray = (indexA, indexB, edgesArrayOutput) =>{
+  return edgesArrayOutput[indexA]? edgesArrayOutput[indexA].find( edge => edge[0]===indexB) : undefined;
+};
+const _setNewEdgeInNode = (newEdgeArray, nodeIndex, edgesArrayOutput) =>{
+  if(_isFirstNodeEdge(nodeIndex, edgesArrayOutput)){
+    edgesArrayOutput[nodeIndex] = [];
+  }
+  edgesArrayOutput[nodeIndex].push(newEdgeArray);
+};
+const _setLowerPriceInEdge = (edgeIndex, newPrice, nodeIndex, edgesArrayOutput) =>{
+  const edge= edgesArrayOutput[nodeIndex][edgeIndex];
+  edgesArrayOutput[nodeIndex][edgeIndex]=[edge[0], newPrice];
+}
+const _setEdgeInArray = (routesObjWithIndex, edgesArrayOutput)=>{
+  const {indexOrigin, indexTarget, price:stringPrice} = routesObjWithIndex;
+  const price = parseFloat(stringPrice);
+
+  const edgeOrigintoTarget= _getEdgeNodeIndexToNodeIndexInArray(indexOrigin, indexTarget, edgesArrayOutput);
+
+  if(_isNewEdge(edgeOrigintoTarget)){
+    _setNewEdgeInNode([indexTarget, price], indexOrigin, edgesArrayOutput);
+  }else{
+    if(_isPriceAMoreExpensiveThanPriceB(edgeOrigintoTarget[1], price)){
+      const edgeOrigintoTargetIndex = _getEdgeIndexInArray(edgeOrigintoTarget, indexOrigin, edgesArrayOutput);
+        _setLowerPriceInEdge(edgeOrigintoTargetIndex, price, indexOrigin, edgesArrayOutput);
+    }
+  }
+}
 
--- tests/unity/app/infrastructure/functional/routeFunctional.test.js
+++ tests/unity/app/infrastructure/functional/routeFunctional.test.js
@@ -15,3 +15,48 @@ describe('Validation of the convertion of route string to array.', () => {
     expect(stringArray[0]).toBe('ABC');
   });
 });
+
+describe('Validation of the convertion of routes array to adjacency list.', () => {
+  it('Should be able to convert a routes array to adjacency list.', () => {
+    const routesArray = [
+      [ 'V1', 'V2', '4' ],
+      [ 'V2', 'V3', '2' ],
+      [ 'V3', 'V1', '3' ],
+      [ 'V1', 'V2', '1' ]
+    ]
+
+    const routesAdjacencyList = routeFunctional.getConvertRoutesArrayToAdjacencyList(routesArray);
+    const nodesQuantity = routesAdjacencyList.GetNodesQuantity();
+    const edgesQuantity = routesAdjacencyList.GetEdgesQuantity();
+    const nodesV1Index = routesAdjacencyList.getNodeIndex('V1');
+    const nodesV2Index = routesAdjacencyList.getNodeIndex('V2');
+    const nodesV3Index = routesAdjacencyList.getNodeIndex('V3');
+
+    expect(nodesQuantity).toBe(3);
+    expect(edgesQuantity).toBe(3);
+
+    expect(nodesV1Index).toBeGreaterThanOrEqual(0);
+    expect(routesAdjacencyList.Nodes[nodesV1Index]).toBe('V1');
+    expect(routesAdjacencyList.Edges[nodesV1Index]).toHaveLength(2);
+    expect(routesAdjacencyList.Edges[nodesV1Index][0][0]).toBe(nodesV2Index);
+    expect(routesAdjacencyList.Edges[nodesV1Index][0][1]).toBe(1);
+    expect(routesAdjacencyList.Edges[nodesV1Index][1][0]).toBe(nodesV3Index);
+    expect(routesAdjacencyList.Edges[nodesV1Index][1][1]).toBe(3);
+
+    expect(nodesV2Index).toBeGreaterThanOrEqual(0);
+    expect(routesAdjacencyList.Nodes[nodesV2Index]).toBe('V2');
+    expect(routesAdjacencyList.Edges[nodesV2Index]).toHaveLength(2);
+    expect(routesAdjacencyList.Edges[nodesV2Index][0][0]).toBe(nodesV1Index);
+    expect(routesAdjacencyList.Edges[nodesV2Index][0][1]).toBe(1);
+    expect(routesAdjacencyList.Edges[nodesV2Index][1][0]).toBe(nodesV3Index);
+    expect(routesAdjacencyList.Edges[nodesV2Index][1][1]).toBe(2);
+
+    expect(nodesV3Index).toBeGreaterThanOrEqual(0);
+    expect(routesAdjacencyList.Nodes[nodesV3Index]).toBe('V3');
+    expect(routesAdjacencyList.Edges[nodesV3Index]).toHaveLength(2);
+    expect(routesAdjacencyList.Edges[nodesV3Index][0][0]).toBe(nodesV2Index);
+    expect(routesAdjacencyList.Edges[nodesV3Index][0][1]).toBe(2);
+    expect(routesAdjacencyList.Edges[nodesV3Index][1][0]).toBe(nodesV1Index);
+    expect(routesAdjacencyList.Edges[nodesV3Index][1][1]).toBe(3);
+  });
+});
